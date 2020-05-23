be2493f2f1980a65364bf8085d522b301188fa93
Frank von Eitzen <fveitzen@gmail.com>
2020-05-23 13:58:22 +0200

    Created interactions for succesfull subscription

--- js/interactions.js
+++ js/interactions.js
@@ -1,8 +1,12 @@
-// Smooth scrolling when clicking an anchor link
+var _user = null;
+
 $(document).ready(function(){
 
-	// Klick auf eien Link, der eine Raute (# = Anker) enthält
+    $('#registrationSuccess').hide();
+
+	// Smooth Scrolling
 	$('a[href^="#"]').on('click',function(e) {
+
 		e.preventDefault();
 
 		var target = this.hash;
@@ -13,6 +17,40 @@ $(document).ready(function(){
 		}, 500, 'swing', function () {
 			window.location.hash = target;
 		});
-	});
+    });
+
+    const form = $('#registration');
+    form.submit(function(e) {
+
+        // prevent Default functionality
+        e.preventDefault();
+        form.addClass('was-validated');
+
+        _user = {
+            firstName: $('#firstname').val(),
+            lastName: $('#lastname').val(),
+            email: $('#email').val(),
+            agreement: $('#agreement').is(':checked')
+        };
+        if (verifyNewsletterForm()) {
+            form.hide();
+            $('#registrationRequest').hide();
+            $('#registrationSuccess').show();
+        };
+
+    });
+
 });
+
+function verifyNewsletterForm() {
+
+    // Reset all field to be true
+    var valid = (_user.firstName.trim().length >= 3) 
+        && (_user.lastName.trim().length >= 3)
+        && (_user.email.trim().length >= 3) 
+        && _user.agreement;
+        
+    return valid;
+
+}
  
